a29b0d1632a6fb48df10c608d13b59de999e5862
emanuellegdes <emanuellegdes@gmail.com>
2023-10-24 15:09:05 -0300

    isolando os arquivos html na pasta templates

--- app/galeria/views.py
+++ app/galeria/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>PAGINA INICIAL</h1>')
+    return render(request, 'index.html')
