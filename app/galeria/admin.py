7f17646ef37d8f56149c008739023bbef1c94c12
emanuellegdes <emanuellegdes@gmail.com>
2023-11-03 16:47:02 -0300

    modelo do banco de dados implementado

--- app/galeria/admin.py
+++ app/galeria/admin.py
@@ -7,5 +7,51 @@ class ListandoCategorias(admin.ModelAdmin):
     search_fields = ("nome_categoria",)
     list_per_page = 10
 
+class ListandoProdutos(admin.ModelAdmin):
+    list_display = ("id_produto", "nome_produto", "categoria")
+    list_display_links = ("id_produto", "nome_produto")
+    search_fields = ("nome_produto",)
+    list_filter = ("categoria",)
+    list_per_page = 10
+
+class ListandoClientes(admin.ModelAdmin):
+    list_display = ("id_cliente", "nome_cliente", "e_mail")
+    list_display_links = ("id_cliente", "nome_cliente")
+    search_fields = ("nome_cliente",)
+    list_per_page = 10
+
+class ListandoPedidos(admin.ModelAdmin):
+    list_display = ("id_pedido", "cliente", "data_pedido", "valor_total")
+    list_display_links = ("id_pedido",)
+    search_fields = ("id_pedido", "cliente", "data_pedido", "valor_total")
+    list_filter = ("cliente", "data_pedido", "valor_total")
+    list_per_page = 10
+
+class ListandoOrcamentos(admin.ModelAdmin):
+    list_display = ("id_orcamento", "cliente", "data_orcamento", "pedido")
+    list_display_links = ("id_orcamento",)
+    search_fields = ("id_orcamento", "cliente", "data_orcamento", "pedido")
+    list_filter = ("cliente", "data_orcamento", "pedido")
+    list_per_page = 10
+
+class ListandoPedidosDetalhes(admin.ModelAdmin):
+    list_display = ("id", "pedido", "produto", "valor_unitario", "quantidade")
+    list_display_links = ("id", "pedido")
+    search_fields = ("pedido", "produto")
+    list_filter = ("pedido", "produto")
+    list_per_page = 10
+
+class ListandoOrcamentosDetalhes(admin.ModelAdmin):
+    list_display = ("id", "orcamento", "produto", "valor_unitario", "quantidade")
+    list_display_links = ("id", "orcamento")
+    search_fields = ("orcamento", "produto")
+    list_filter = ("orcamento", "produto")
+    list_per_page = 10
+
 admin.site.register(Categoria, ListandoCategorias)
-admin.site.register(Produtos)
+admin.site.register(Produto, ListandoProdutos)
+admin.site.register(Cliente, ListandoClientes)
+admin.site.register(Pedido, ListandoPedidos)
+admin.site.register(Orcamento, ListandoOrcamentos)
+admin.site.register(PedidoDetalhe, ListandoPedidosDetalhes)
+admin.site.register(OrcamentoDetalhe, ListandoOrcamentosDetalhes)
--- app/galeria/models.py
+++ app/galeria/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from datetime import datetime
 
-# Create your models here.
 class Categoria(models.Model):
     id_categoria = models.AutoField(primary_key=True)
     nome_categoria = models.CharField(max_length=50, null=False, blank=False)
@@ -12,11 +11,56 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nome_categoria
     
-class Produtos(models.Model):
+class Produto(models.Model):
     id_produto = models.AutoField(primary_key=True)
     nome_produto = models.CharField(max_length=50, null=False, blank=False)
-    id_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     foto = models.ImageField(upload_to="fotos/", blank=True)
 
     def __str__(self):
         return self.nome_produto
+
+class Cliente(models.Model):
+    id_cliente = models.AutoField(primary_key=True)
+    nome_cliente = models.CharField(max_length=150, null=False, blank=False)
+    e_mail = models.EmailField(max_length=254)
+    senha = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.nome_cliente
+    
+class Pedido(models.Model):
+    id_pedido = models.AutoField(primary_key=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True)
+    data_pedido = models.DateField(default=datetime.now, blank=False)
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return str(self.id_pedido)
+
+class Orcamento(models.Model):
+    id_orcamento = models.AutoField(primary_key=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    data_orcamento = models.DateField(default=datetime.now, blank=False)
+    pedido = models.ForeignKey(Pedido, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.id_orcamento)
+    
+class PedidoDetalhe(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.SET_NULL, null=True)
+    valor_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id)
+
+class OrcamentoDetalhe(models.Model):
+    orcamento = models.ForeignKey(Orcamento, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.SET_NULL, null=True)
+    valor_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id)
