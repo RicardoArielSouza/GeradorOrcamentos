1b55fe2f91d193371274be51fa311bd698f724e7
emanuellegdes <emanuellegdes@gmail.com>
2023-11-03 08:38:20 -0300

    ajustando admin

--- app/galeria/admin.py
+++ app/galeria/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from galeria.models import *
 
 class ListandoCategorias(admin.ModelAdmin):
-    pass
+    list_display = ("id_categoria", "nome_categoria", "preco_ate_cem", "preco_ate_duzentos", "preco_acima_duzentos")
+    list_display_links = ("id_categoria", "nome_categoria")
+    search_fields = ("nome_categoria",)
+    list_per_page = 10
 
-admin.site.register(Categoria)
+admin.site.register(Categoria, ListandoCategorias)
 admin.site.register(Produtos)
